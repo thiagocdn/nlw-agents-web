acb6ef5992f6b0c21154f49e07245245a613472a
Thiago Cedran <thiagocdn@gmail.com>
2025-07-13 21:48:59 -0300

    feat: integrated form to create room

--- src/components/create-room-form.tsx
+++ src/components/create-room-form.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
+import { useCreateRoom } from '@/http/use-create-room';
 import { Button } from './ui/button';
 import {
   Card,
@@ -28,6 +29,8 @@ const createRoomSchema = z.object({
 type CreateRoomFormData = z.infer<typeof createRoomSchema>;
 
 export function CreateRoomForm() {
+  const { mutateAsync: createRoom } = useCreateRoom();
+
   const createRoomForm = useForm<CreateRoomFormData>({
     resolver: zodResolver(createRoomSchema),
     defaultValues: {
@@ -36,8 +39,9 @@ export function CreateRoomForm() {
     },
   });
 
-  function handleCreateRoom(data: CreateRoomFormData) {
-    console.log(data);
+  async function handleCreateRoom({ name, description }: CreateRoomFormData) {
+    await createRoom({ name, description });
+    createRoomForm.reset();
   }
 
   return (
